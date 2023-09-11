2f7d568dc9d2ae6be96d81a366289cf8f494aaf0
AlexDevelop1991 <alexdevelop1991@gmail.com>
2023-09-11 17:45:20 +0300

    git

---
+++ Основные_команды_в_терминале.py
@@ -0,0 +1,17 @@
+# Переход в папку : cd~/Desktop
+
+# Создание новой папки : mkdir my-project(названия папки)
+
+# Переход в папку : cd my-project
+
+# Создание файла с текстом : echo "Some text" > file.txt
+
+# Список файлов в папке : ls
+
+# Чтение файла : cat file.txt
+
+# Удаление файла : rm file.txt
+
+# Отображение скрытых папок и файлов (PowerShell Windows) : ls -Force
+
+#  Отображение скрытых папок и файлов (GitBash Windows) : ls -la
